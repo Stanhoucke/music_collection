3481c985c433d218397252d683d64ee776c78784
Stanley Houcke <stanhoucke@gmail.com>
2020-12-08 15:19:34 +0000

    initial commit

---
+++ db/music_collection_manager.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INT
+);
+
+CREATE TABLE albums (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    year INT,
+    artist_id INT REFERENCES artists(id)
+);
